c0f153d5026e20c546244a640c83183a8058c6c7
Erin Hinson <ehinson@treetopllc.com>
2020-02-17 20:36:25 -0700

    testing jest testing

---
+++ src/views/components/__tests__/App.test.js
@@ -0,0 +1,21 @@
+/* eslint-disable no-undef */
+import React from "react";
+import { shallow } from "enzyme";
+
+// Components
+import App from "../App";
+
+function setup() {
+  const props = {
+    hasToken: true
+  };
+  const wrapper = shallow(<App />);
+  return { wrapper, props };
+}
+
+describe("App Test Suite", () => {
+  it("Should match snapshot", () => {
+    const { wrapper } = setup();
+    expect(wrapper.find("App")).toMatchSnapshot();
+  });
+});
